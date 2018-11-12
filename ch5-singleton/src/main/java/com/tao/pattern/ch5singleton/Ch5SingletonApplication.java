570dbd74c7dd0bd51bd71345f16e32d934f632ac
yanjiantao <boolean.yan@gmail.com>
2018-11-12 14:53:49 +0800

    update pom

---
+++ ch5-singleton/src/main/java/com/tao/pattern/ch5singleton/Ch5SingletonApplication.java
@@ -0,0 +1,12 @@
+package com.tao.pattern.ch5singleton;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ch5SingletonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ch5SingletonApplication.class, args);
+	}
+}
---
+++ ch5-singleton/src/test/java/com/tao/pattern/ch5singleton/Ch5SingletonApplicationTests.java
@@ -0,0 +1,16 @@
+package com.tao.pattern.ch5singleton;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Ch5SingletonApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
